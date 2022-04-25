e6231b1863b5b47035edb5f760f0110a70f2a3bd
Oscar-Wohlfarhrt <oscarwohlfarhrt@gmail.com>
2022-04-25 13:11:09 -0300

    Update tpstructs.h
    Se añadio la variable next a todas las estructuras

--- libs/tpstructs.h
+++ libs/tpstructs.h
@@ -9,6 +9,7 @@ typedef struct Clientes{
     struct tm fechaNacimiento;
     struct tm ultimaActividad;
     struct tm fechaBaja;
+    struct Clientes *next;
 }Clientes;
 
 typedef struct Profesores{
@@ -16,6 +17,7 @@ typedef struct Profesores{
     char apellido[50];
     long dni;
     char telefono[20];
+    struct Profesores *next;
 }Profesores;
 
 typedef struct Actividades{
@@ -24,17 +26,20 @@ typedef struct Actividades{
     int sucursal;
     int turno;
     long profesorDNI;
+    struct Actividades *next;
 }Actividades;
 
 typedef struct Turnos{
     int dias[7];
     struct tm horarioInicio;
     struct tm horarioFin;
+    struct Turnos *next;
 }Turnos;
 
 typedef struct ClientesActivTurno{
     long dni;
     char actividad[50];
+    struct ClientesActivTurno *next;
 }ClientesActivTurno;
 
 typedef struct ClientesPagos{
@@ -42,9 +47,11 @@ typedef struct ClientesPagos{
     float monto;
     struct tm fechaEmision;
     struct tm fechaPago;
+    struct ClientesPagos *next;
 }ClientesPagos;
 
 typedef struct Reservas{
     long dni;
     char actividad[50];
+    struct Reservas *next;
 }Reservas;
