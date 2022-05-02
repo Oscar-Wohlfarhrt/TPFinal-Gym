a12b4a905dd62e71689a19fcf131c9b557489858
Oscar-Wohlfarhrt <oscarwohlfarhrt@gmail.com>
2022-05-02 19:10:12 -0300

    Update tpstructs.h
    se prototiparon los structs

--- libs/tpstructs.h
+++ libs/tpstructs.h
@@ -5,7 +5,22 @@
 #include <ctype.h>
 #include "../libs/cmenus.h"
 
-typedef struct Clientes{
+struct Clientes;
+typedef struct Clientes Clientes;
+struct Profesores;
+typedef struct Profesores Profesores;
+struct Turnos;
+typedef struct Turnos Turnos;
+struct Actividades;
+typedef struct Actividades Actividades;
+struct ClientesActivTurno;
+typedef struct ClientesActivTurno ClientesActivTurno;
+struct ClientesPagos;
+typedef struct ClientesPagos ClientesPagos;
+struct Reservas;
+typedef struct Reservas Reservas;
+
+struct Clientes{
     char nombre[50];
     char apellido[50];
     long dni;
@@ -13,49 +28,49 @@ typedef struct Clientes{
     struct tm fechaNacimiento;
     struct tm ultimaActividad;
     struct tm fechaBaja;
-    struct Clientes *next;
-}Clientes;
+    Clientes *next;
+};
 
-typedef struct Profesores{
+struct Profesores{
     char nombre[50];
     char apellido[50];
     long dni;
     char telefono[20];
     struct Profesores *next;
-}Profesores;
+};
 
-typedef struct Turnos{
+struct Turnos{
     char dias;
     struct tm horarioInicio;
     struct tm horarioFin;
     struct Turnos *next;
-}Turnos;
+};
 
-typedef struct Actividades{
+struct Actividades{
     char nombre[50];
     int cupo;
     int sucursal;
     Turnos turno;
     long profesorDNI;
     struct Actividades *next;
-}Actividades;
+};
 
-typedef struct ClientesActivTurno{
+struct ClientesActivTurno{
     long dni;
     char actividad[50];
     struct ClientesActivTurno *next;
-}ClientesActivTurno;
+};
 
-typedef struct ClientesPagos{
+struct ClientesPagos{
     long dni;
     float monto;
     struct tm fechaEmision;
     struct tm fechaPago;
     struct ClientesPagos *next;
-}ClientesPagos;
+};
 
-typedef struct Reservas{
+struct Reservas{
     long dni;
     char actividad[50];
     struct Reservas *next;
-}Reservas;
+};
