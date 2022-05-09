eadd4f2efd9d73e9ffc1c142d3f553aa56780979
Oscar-Wohlfarhrt <oscarwohlfarhrt@gmail.com>
2022-05-09 18:23:06 -0300

    funciones estandar

--- main.c
+++ main.c
@@ -4,6 +4,23 @@
 #include "libs/Handlers/actHandler.h"
 //#include "libs/Handlers/turHandler.h"
 
+/* 
+//si se busca por mas de una variable
+//reemplazar [tipo] por [struct]
+
+[struct] es el tipo del struct (Clientes, Turnos, etc)
+[tipo] es un tipo de variable (int, long, etc)
+
+void Load([struct] **list)
+void Save([struct] *list)
+void Insert([struct] **node, [struct] **list)
+int Remove([tipo] value, [struct] **list) 
+[struct] *Find([tipo] value,[struct] *list)
+int Replace([tipo] value,[struct] *new,[struct] **list)
+
+
+*/
+
 int main(int argc, char **args)
 {
     nActividad *L;
