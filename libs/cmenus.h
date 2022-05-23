dd39a4e6728c6515a4fa5f60b83e98b5e3287c80
Oscar-Wohlfarhrt <oscarwohlfarhrt@gmail.com>
2022-05-23 17:59:21 -0300

    linux support added

--- libs/cmenus.h
+++ libs/cmenus.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "strext.h"
 
 #ifdef __unix__
 #define cls "clear"
---
+++ libs/strext.h
@@ -0,0 +1,25 @@
+#ifndef _strext
+#define _strext
+#include <string.h>
+#include <ctype.h>
+
+#ifdef __linux__
+char *strlwr(char *text)
+{
+    for (int i = 0; i < strlen(text); i++)
+    {
+        text[i] = tolower(text[i]);
+    }
+    return text;
+}
+
+char *strupr(char *text)
+{
+    for (int i = 0; i < strlen(text); i++)
+    {
+        text[i] = toupper(text[i]);
+    }
+    return text;
+}
+#endif
+#endif
