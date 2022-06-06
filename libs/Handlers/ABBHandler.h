a254f2456e6a85455e8249d1f97078b379f9a99d
EmmaVogler <emmavogler224@gmail.com>
2022-06-06 13:28:54 -0300

    Pequeño fix, caso exclusivo de strcmp

--- libs/Handlers/ABBHandler.h
+++ libs/Handlers/ABBHandler.h
@@ -5,24 +5,23 @@ ABClientes *root = NULL;
 ABClientes *insert(ABClientes *raiz, ABClientes *nodo);
 ABClientes *borrarArbol(ABClientes *raiz);
 void cargarEnABB(Clientes *cli);
-short cmp(char *str1, char *str2);
 
 ABClientes *insert(ABClientes *raiz, ABClientes *nodo)
 {
     if (!raiz)
         return nodo;
-    if((strcmp(nodo->nombre, raiz->nombre)<0 || strcmp(nodo->apellido, raiz->apellido)<0)){
+    if((strcmp(nodo->nombre, raiz->nombre)<0 && strcmp(nodo->apellido, raiz->apellido)<0)
+    ||(strcmp(nodo->nombre, raiz->nombre)>0 && strcmp(nodo->apellido, raiz->apellido)<0)
+    ||(strcmp(nodo->nombre, raiz->nombre)==0 && strcmp(nodo->apellido, raiz->apellido)<0)
+    ||(strcmp(nodo->nombre, raiz->nombre)<0 && strcmp(nodo->apellido, raiz->apellido)==0)){
         raiz->izq = insert(raiz->izq, nodo);
     }
-    else if((strcmp(nodo->nombre, raiz->nombre)>0 || strcmp(nodo->apellido, raiz->apellido)>0)){
+    else if((strcmp(nodo->nombre, raiz->nombre)>0 && strcmp(nodo->apellido, raiz->apellido)>0)
+    ||(strcmp(nodo->nombre, raiz->nombre)<0 && strcmp(nodo->apellido, raiz->apellido)>0)
+    ||(strcmp(nodo->nombre, raiz->nombre)==0 && strcmp(nodo->apellido, raiz->apellido)>0)
+    ||(strcmp(nodo->nombre, raiz->nombre)>0 && strcmp(nodo->apellido, raiz->apellido)==0)){
         raiz->der = insert(raiz->der, nodo);
     }
-    /*if (strcmp(nodo->nombre, raiz->nombre) <= 0 && strcmp(nodo->apellido, raiz->apellido) <= 0)
-        raiz->izq = insert(raiz->izq, nodo);
-    else if (strcmp(nodo->nombre, raiz->nombre) >= 0 && strcmp(nodo->apellido, raiz->apellido) > 0)
-        raiz->der = insert(raiz->der, nodo);
-    else
-        raiz->der = insert(raiz->der, nodo);*/
     return raiz;
 }
 void cargarEnABB(Clientes *cli)
@@ -48,30 +47,4 @@ ABClientes *borrarArbol(ABClientes *raiz)
         return NULL;
     }
     return raiz;
-}
-short cmp(char *str1, char *str2)
-// aca se comparan los nombres y apellidos, para eso se deben concatenar el nombre y apellido y pasarlos por parametro
-{
-    /*
-    0 = las strings son iguales
-    1 = la string 1 es mas chica
-    2 = la string 2 es mas chica
-    */
-    strlwr(str1);
-    strlwr(str2);
-    short int sel = 0;
-    for (int i = 0; i < strlen(str1); i++)
-    {
-        if (str1[i] < str2[i])
-        {
-            sel = 1;
-            break;
-        }
-        else if (str1[i] > str2[i])
-        {
-            sel = 2;
-            break;
-        }
-    }
-    return sel;
 }
